7a69364f5dd67213110191af07b4b30e56a1f6a5
Bivis-s <typoborov@yandex.by>
2021-03-26 03:56:40 +0300

    String and Xpath Utils classes were added

---
+++ src/main/java/parser/utils/StringUtils.java
@@ -0,0 +1,31 @@
+package parser.utils;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class StringUtils {
+    private StringUtils() {
+    }
+
+
+    /**
+     * Cut out all sub strings from main string.
+     * For example, if mainString = "the string" and subString = "the " then "string" will return
+     *
+     * @param mainString the main string
+     * @param subString  the sub string
+     * @return the string with no sub strings
+     */
+    public static String cutOutSubStrings(String mainString, String subString) {
+        log.trace("Cut out all substrings '" + subString + "' from string '" + mainString + "'");
+        while (mainString.contains(subString)) {
+            int indexOfSubString = mainString.indexOf(subString);
+            int lastIndexOfSubString = indexOfSubString + subString.length();
+            String firstPartOfString = mainString.substring(0, indexOfSubString);
+            String secondPartOfString = mainString.substring(lastIndexOfSubString);
+            mainString = firstPartOfString + secondPartOfString;
+            log.trace("Result of cutting out '" + mainString + "'");
+        }
+        return mainString;
+    }
+}
